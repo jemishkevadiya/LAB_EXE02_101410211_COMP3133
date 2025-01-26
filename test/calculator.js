5a9aaa8f413d39af56b059b67229b4a6e43b1fc7
jemish kevadiya <jemishkevadiya007@gmail.com>
2025-01-25 21:06:54 -0500

    update mistake in assigning values

--- test/calculator.js
+++ test/calculator.js
@@ -3,43 +3,42 @@ const { expect } = require("chai");
 
 describe("Calculator Tests", () => {
     it("add(1, 1) expected result 2 PASS", () => {
-        const result = calculator.add(5, 2);
-        expect(result).to.equal(7);
+        const result = calculator.add(1, 1);
+        expect(result).to.equal(2);
     });
 
     it("add(1, 1) expected result 3 FAIL", () => {
-        const result = calculator.add(5, 2);
-        expect(result).to.equal(8);
-    });
-
-    it("sub(1, 1) expected result 0 PASS", () => {
-        const result = calculator.sub(5, 2);
+        const result = calculator.add(1, 1);
         expect(result).to.equal(3);
     });
 
+    it("sub(1, 1) expected result 0 PASS", () => {
+        const result = calculator.sub(1, 1);
+        expect(result).to.equal(0);
+    });
+
     it("sub(1, 1) expected result 5 FAIL", () => {
-        const result = calculator.sub(5, 2);
+        const result = calculator.sub(1, 1);
         expect(result).to.equal(5);
     });
 
     it("mul(1, 1) expected result 1 PASS", () => {
-        const result = calculator.mul(5, 2);
-        expect(result).to.equal(10);
+        const result = calculator.mul(1, 1);
+        expect(result).to.equal(1);
     });
 
     it("mul(1, 1) expected result 2 FAIL", () => {
-        const result = calculator.mul(5, 2);
-        expect(result).to.equal(12);
+        const result = calculator.mul(1, 1);
+        expect(result).to.equal(2);
     });
 
     it("div(1, 1) expected result 1 PASS", () => {
-        const result = calculator.div(10, 2);
-        expect(result).to.equal(5);
+        const result = calculator.div(1, 1);
+        expect(result).to.equal(1);
     });
 
     it("div(1, 1) expected result 2 FAIL", () => {
-        const result = calculator.div(10, 2);
+        const result = calculator.div(1, 1);
         expect(result).to.equal(2);
     });
 });
-
